29a633d1eb5837146d87e06d85c5122a9661fac4
DuDeXsUraj <116436325+DuDeXsUraj@users.noreply.github.com>
2023-02-05 21:41:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Review from './Components/Review';
+
+function App() {
+  return (
+    <main>
+    <div className="App">
+     <Review/>
+    </div>
+    </main>
+  );
+}
+
+export default App;
